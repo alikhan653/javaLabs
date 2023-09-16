c75bbae9226ef4947a535034d1e5455eb845bc69
Alikhan <alihan65321@gmail.com>
2023-09-16 09:35:06 +0600

    Initial commit

---
+++ src/main/java/kz/iitu/javaLabs/JavaLabsApplication.java
@@ -0,0 +1,13 @@
+package kz.iitu.javaLabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/iitu/javaLabs/JavaLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.javaLabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
